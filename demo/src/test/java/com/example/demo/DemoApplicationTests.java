bcd6842c7ce655f6bcff9b8f19449b76a7cc0b22
goram <goram@192.168.1.37>
2022-02-17 10:55:56 +0530

    test cases.

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,20 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		assertEquals(0, 0);
+	}
+	
+	@Test
+	void secondTest() {
+		
+		assertEquals(0, 0);
+	}
+	
+	@Test
+	void thirdTest() {
+		
+		assertEquals(0, 0);
 	}
 
 }
